7c8d6af50dba93c72ad018369a63955762bccac0
Jery Ramírez <rafa85jogo@gmail.com>
2019-02-22 10:02:30 -0600

    Create Views

---
+++ app/src/main/java/com/example/platzigram/Views/CreateCountActivity.java
@@ -0,0 +1,13 @@
+package com.example.platzigram.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateCountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_count);
+    }
+}
