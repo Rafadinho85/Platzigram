d17320e460ae7428ada4ca194ade729f7d4e9cba
Jery Ramírez <rafa85jogo@gmail.com>
2019-02-26 10:35:04 -0600

    Maquetando nuestro proyecto

--- app/src/main/java/com/example/platzigram/Views/CreateCountActivity.java
+++ app/src/main/java/com/example/platzigram/Views/CreateCountActivity.java
@@ -3,6 +3,8 @@ package com.example.platzigram.Views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.platzigram.R;
+
 public class CreateCountActivity extends AppCompatActivity {
 
     @Override
